b8fd59ce9330d2ff9a0633680763bb7bf0702d9e
javadkavossi <jkavossi@gmail.com>
2024-05-05 19:48:48 +0330

    update toolkit and fix error

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <>
-    <html lang="en">
-      <body className={inter.className }><Providers>{children}</Providers></body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}><Providers>{children}</Providers></body>
+      </html>
     </>
   );
 }
